809658e9cadf079b473d6ebd46d25335ed660425
din0x <superrobertxd52@gmail.com>
2024-05-14 00:29:55 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std::{fs::File, io::Read, path::PathBuf};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    file: PathBuf,
+    #[arg(short, long)]
+    cols: Option<u8>,
+    #[arg(short, long)]
+    plain: bool,
+    #[arg(short, long)]
+    bits: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let mut file = File::open(args.file).unwrap();
+    let mut bytes = Vec::new();
+    file.read_to_end(&mut bytes).unwrap();
+
+    let cols = args.cols.unwrap_or(16);
+
+    hex_dump(&bytes, cols as usize, args.plain, args.bits);
+}
+
+fn hex_dump(bytes: &[u8], cols: usize, plain: bool, bits: bool) {
+    for chunk in bytes.chunks(cols) {
+        for (i, byte) in chunk.iter().enumerate() {
+            if bits {
+                print!("{:08b}", byte)
+            } else {
+                print!("{:02x}", byte);
+            }
+
+            if i % 2 == 1 && !plain || bits{
+                print!(" ");
+            }
+        }
+
+        if plain {
+            println!();
+            continue;
+        }
+
+        print!("\x1b[{}G ", cols * 3 - cols / 2);
+
+        for ch in chunk.iter().map(|el| *el as char) {
+            match ch {
+                ' ' => print!("·"),
+                '\r' => print!("\\r"),
+                '\n' => print!("\\n"),
+                _ => print!("{}", ch),
+            }
+        }
+
+        println!()
+    }
+}
